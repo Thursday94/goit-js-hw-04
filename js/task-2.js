8cd244f4db38d443ab0c52c6dfb4a8ad40831e22
Roman <romankovslov94@gmail.com>
2025-08-14 20:35:00 +0200

    add console.log test to task2

--- js/task-2.js
+++ js/task-2.js
@@ -26,3 +26,19 @@ console.log(
     { day: "sunday", calories: 3300 }
   ])
 );
+
+console.log(
+  calcAverageCalories([])
+); 
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+);
